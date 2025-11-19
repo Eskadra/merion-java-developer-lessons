3f48f34886aa73056451935d153db1f26da301e5
Fedor Vyrzhikovskiy <hoyapalmer@gmail.com>
2025-11-19 11:41:36 +0300

    added lesson3

---
+++ src/lesson3/factorial.java
@@ -0,0 +1,21 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+        
+        long factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        
+        System.out.println("Факториал числа " + number + " = " + factorial);
+        
+        scanner.close();
+    }
+}
